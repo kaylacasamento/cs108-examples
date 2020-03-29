4cf6801defeb802aee39be14ecd9dc2ec2b913d2
Kayla Casamento <kaylacas@bu.edu>
2020-03-29 18:17:29 -0400

    Added use of DetailView to display one quote, random quote.

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,9 +1,11 @@
 # quotes/urls.py
 
 from django.urls import path
-from .views import HomePageView # our class definition
+from .views import HomePageView, QuotePageView, RandomQuotePageView # our class definition
 
 urlpatterns = [
     # map the URL (empty string) to the function 'homePageView'
-    path('', HomePageView.as_view(), name='home'), # generic class-base view
+    path('', RandomQuotePageView.as_view(), name="random"), # pick a random quote
+    path('all', HomePageView.as_view(), name='home'), # generic class-based view
+    path('quote/<int:pk>', QuotePageView.as_view(), name='quote'), # show one quote
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Quote
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+import random
 
 class HomePageView(ListView):
     '''Create a subclass of ListView to display all quotes.'''
@@ -11,3 +12,26 @@ class HomePageView(ListView):
     template_name = 'quotes/home.html'
     context_object_name = 'all_quotes_list' # how to find the data in the template file
     
+class QuotePageView(DetailView):
+    '''Show the details for one quote.'''
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+
+class RandomQuotePageView(DetailView):
+    '''Show one quote selected at random.'''
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+
+    # pick one quote at random
+    def get_object(self):
+        '''Return a single instance of a Quote object, selected at random.'''
+
+        # get all quotes
+        all_quotes = Quote.objects.all()
+
+        # pick one, at random
+        r = random.randint(0, len(all_quotes) -1)
+        q = all_quotes[r]
+        return q # return this object
